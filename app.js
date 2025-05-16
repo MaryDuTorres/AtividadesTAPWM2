5d870ac5bf8ed1a82825ada432d7e4876b9b9323
Maria Eduarda Torres <maria.pereira96@fatec.sp.gov.br>
2025-05-16 19:20:45 +0000

    Atividade 16-05

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+const lista = [
+    {id: 1, name: 'John Doe'},
+    {id: 2, name: 'Jane Doe'}
+];
+
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(lista)
+}
+);
+
+app.listen(port, () => {
+    console.log(`Exemplo app listening on port ${port}`);
+
+})
